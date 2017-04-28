0be8e46529f9308619986b42ae5302a84d62f5d3
daheda <da@esokia-webagency.com>
2017-04-28 17:45:27 +0400

    Formulaire add

--- projet/module/Api/src/Api/Controller/IndexController.php
+++ projet/module/Api/src/Api/Controller/IndexController.php
@@ -5,6 +5,9 @@ namespace Api\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
+use Api\Model\Album;
+use Api\Form\AlbumForm;
+
 class IndexController extends AbstractActionController
 {
     protected $albumTable;
@@ -20,6 +23,28 @@ class IndexController extends AbstractActionController
     public function printAction()
     {
         return new ViewModel();
+    }
+    
+    public function addAction()
+    {
+        $form = new AlbumForm();
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $album = new Album();
+            $form->setInputFilter($album->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $album->exchangeArray($form->getData());
+                $this->getAlbumTable()->saveAlbum($album);
+
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('api');
+            }
+        }
+        return array('form' => $form);
     }
 
     /*
